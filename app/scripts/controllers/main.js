b416e17a4caac95e9561b3b48d318e99e2cdf57c
Brett Wiesner <brettwiesner@gmail.com>
2013-10-06 15:39:44 -0400

    lawyerew first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('lawyerewApp')
+  .controller('MainCtrl', ['$scope', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  }]);
